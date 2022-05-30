f75c083d8b7d6c4edf625c4ff289799a020b575b
tsoeunyanemf <tsoeunyanemf@users.noreply.github.com>
2022-05-30 21:53:57 +0200

    Add files via upload

---
+++ create_user.py
@@ -0,0 +1,7 @@
+from moseli_oop_project import *
+def main():
+
+  create_account() #function used when creating new user
+
+if __name__ == "__main__":
+    main()
---
+++ user_login.py
@@ -0,0 +1,7 @@
+from moseli_oop_project import *
+def main():
+  
+  user_login() #function used when logging in
+
+if __name__ == "__main__":
+    main()
